40d9cb39ec1ef0a1295b5cf5a02ade58c3f4a0e9
Ambadas Gote <ambadasgote7@gmail.com>
2025-10-06 19:54:15 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,18 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home",(req, res) => {
-    res.send("Hey, Ambadas Here!");
+// THis will match all HTTP methods (GET, POST, PUT, DELETE, etc.) to /user
+app.use("/user", (req, res) => {
+    res.send("Heellooooooooooo");
 });
 
-
-app.use("/hello",(req, res) => {
-    res.send("Hello Hello server!");
+//  This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName :  "Ambadas" , lastName : "Gote"});
 });
 
+// This will only handle POST call to /user
+app.post("/user",(req, res) => {
+    res.send("Post request called");
+});
+
+// This will only handle DELETE call to /user
+app.delete("/user",(req, res) => {
+    res.send("Delete request called");
+});
+
+// this will match all HTTP methods (GET, POST, PUT, DELETE, etc.) to /test
 app.use("/test",(req, res) => {
     res.send("Hello from server!");
 });
+
 
 app.listen(7777 , () => {
     console.log("Server is running on port 7777...");
